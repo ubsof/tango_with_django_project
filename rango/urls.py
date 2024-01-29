ce2654b5e59800e6cbd89cc9497e403b90c5f419
Anthony Fung <2713718F@student.gla.ac.uk>
2024-01-29 15:16:28 +0000

    Chapter 3 and exercise done

--- rango/urls.py
+++ rango/urls.py
@@ -4,8 +4,6 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    #changed for exercise 4
-    path('index/', views.index, name = 'index'),
-    path('about/', views.about, name ='about')
-
+    path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about')
 ]
--- rango/views.py
+++ rango/views.py
@@ -3,17 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
-
-    #return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    context_dict = {'boldmessage': 'Rango says here is the about page.'}
-    return render(request, 'rango/about.html', context=context_dict)
-
-    #return HttpResponse("Rango says here is the about page. <a href='/rango'>Index</a>")
-
-    
-
-    
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
